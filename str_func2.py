2f05f7ce2bb179d44838560a9ace4530bce65dba
avkanavina <a.kanavina@skyeng.ru>
2023-07-02 14:44:38 +0500

    Add func str_func

--- str_func2.py
+++ str_func2.py
@@ -1,2 +1,8 @@
 def upper_func(new_string):
+	""" Приведение строки к верхнему регистру """
 	return new_string.upper()
+
+
+def title_func(new_string):
+	""" Приведение к верхнему регистру первые буквы"""
+	return new_string.title()
